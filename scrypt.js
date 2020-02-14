3719f874a30d1c80932b1b59f7b7ede7065477ee
Vitalii-Hapon <vitali.gapon@gmail.com>
2020-02-14 18:25:05 +0200

    Update 14-02

--- scrypt.js
+++ scrypt.js
@@ -4,23 +4,43 @@ $(document).ready(function () {
     /** Click on label */
     $(".radio-check").click(function () {
         $(".names_label").hide();
-        $(".names_input").val("");
-        const input_type = '.For' + $(this).children('span').text();
+        $(".class").show();
+        $(".names_input").val(``);
+        $('#html').val(``);
 
-        $('.inputs_names').find(input_type).show();
+        switch ($(this).children('span').text()) {
+            case 'Button':
+                $('.name').show();
+                break;
+            case 'Input':
+                $('.name, .placeholder').show();
+                break;
+            case 'Textarea':
+                $('.placeholder').show();
+                break;
+            case 'Radio':
+                $('.value, .name').show();
+                break;
+            case 'Checkbox':
+                $('.value, .name').show();
+                break;
+        }
+
     });
 
-    $('.generate').click(function () {
+    function generate_code() {
+
         const input_type = $("input:radio:checked").val();
+        const class_name = $('.class_text').val();
         const value = $('.value_text').val();
         const name = $('.name_text').val();
         const placeholder = $('.placeholder_text').val();
 
-        const button = `<button class="button">${value}</button>`;
-        const input = '<label class="label"><span>' + value + '</span><input class="input-text" type="text" placeholder="' + placeholder + '"></label>';
-        const textarea = '<textarea name="textarea_name" id="textarea" cols=100" rows="100" placeholder="' + placeholder + '"></textarea>';
-        const radio = '<label class="label"><input class="input-radio" type="radio" name="' + name + '"><span>' + value + '</span></label>';
-        const checkbox = '<label class="label"><input class="input-checkbox" type="checkbox" name="' + name + '"><span>' + value + '</span></label>';
+        const button = `<form><button class="${class_name}">${name}</button></form>`;
+        const input = `<form><label class="${class_name}">${name}<input class="${class_name}_input" type="text" placeholder="${placeholder}"></label></form>`;
+        const textarea = `<form><textarea name="${class_name}" id="${class_name}_textarea" cols=100" rows="100" placeholder="${placeholder}"></textarea></form>`;
+        const radio = `<form><label class="${class_name}"><input class="${class_name}_radio" type="radio" name="${name}">${value}</label></form>`;
+        const checkbox = `<form><label class="${class_name}"><input class="${class_name}_checkbox" type="checkbox" name="${name}">${value}</label></form>`;
 
         switch (input_type) {
             case 'Button':
@@ -39,5 +59,10 @@ $(document).ready(function () {
                 $('#html').val(checkbox);
                 break;
         }
-    });
+    }
+
+    $('.generate').click(generate_code);
+    $(document).keydown (function(e){
+            if(e.keycode == 13) {console.log(1)}
+        });
 });
