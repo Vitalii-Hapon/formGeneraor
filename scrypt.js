5356da0f5d7a1893db2260d776b4c9dfb17a1d97
Vitalii-Hapon <vitali_gapon@gmail.com>
2020-02-23 18:21:04 +0200

    UPGRATE 2020.02.23

--- scrypt.js
+++ scrypt.js
@@ -37,6 +37,8 @@ $(document).ready(function () {
     }
 
     let classText, valueText, nameText, placeholderText, button, input, textarea, radio, checkbox;
+    const buttonRemove = `<button class="remove button">-</button>`;
+        // + `<br class="end">`;
 
     function getName() {
         function checkNameValue(className) {
@@ -52,16 +54,17 @@ $(document).ready(function () {
         nameText = checkNameValue('name');
         placeholderText = checkNameValue('placeholder');
 
-        button = `<button class="${classText}_button">${nameText}</button>`;
+        button = `<button class="${classText}_button">${nameText}</button>` + buttonRemove;
         input = `<label class="${classText}_label">` +
             `${nameText}<input class="${classText}_input" type="text" placeholder="${placeholderText}">` +
-            `</label>`;
-        textarea = `<textarea name="${nameText}" id="${classText}" cols=3" rows="3" placeholder="${placeholderText}">` +
-            `</textarea>`;
+            `</label>` + buttonRemove;
+        textarea = `<label class="${classText}_label">` +
+            `<textarea name="${nameText}" id="${classText}" cols=20" rows="1" placeholder="${placeholderText}">` +
+            `</textarea></label>` + buttonRemove;
         radio = `<label class="${classText}_label">` +
-            `<input class="${classText}_radio" type="radio" name="${nameText}">${valueText}</label>`;
+            `<input class="${classText}_radio" type="radio" name="${nameText}">${valueText}</label>` + buttonRemove;
         checkbox = `<label class="${classText}_label">` +
-            `<input class="${classText}_checkbox" type="checkbox" name="${nameText}">${valueText}</label>`;
+            `<input class="${classText}_checkbox" type="checkbox" name="${nameText}">${valueText}</label>` + buttonRemove;
     }
 
     function addInput() {
@@ -70,47 +73,49 @@ $(document).ready(function () {
 
         switch (inputType) {
             case 'Button':
-                $('.form').append(button);
+                $('#results').append(button);
                 break;
             case 'Input':
-                $('.form').append(input);
+                $('#results').append(input);
                 break;
             case 'Textarea':
-                $('.form').append(textarea);
+                $('#results').append(textarea);
                 break;
             case 'Radio':
-                $('.form').append(radio);
+                $('#results').append(radio);
                 break;
             case 'Checkbox':
-                $('.form').append(checkbox);
+                $('#results').append(checkbox);
                 break;
             default:
-                $('.form').append(``);
+                $('#results').append(``);
         }
     }
 
     $('.add').click(function () {
         getName();
         addInput()
-        console.log(`1`)
-    });
-
-    $('.remove').click(function () {
-        $('.form').children().last().remove();
     });
 
     $('.generate').click(function () {
-        let result = $('.results').html();
+        let result = `<form class="form">` + $('.results').html() + `</form>`;
         $('#html').val(result);
     });
 
     $('.reset').click(function () {
         clearInputsFopdrs();
-        $('.form').html(``);
+        $('.results').html(``);
         $('#html').val(``);
-
+        $("input:radio").prop('checked',false);
+        // // $("input:radio").removeAttr("checked");
+        // console.log($("input:radio")).val(``);
+        // $('input[name=Choose]').attr('checked',false);
     });
 
+    $(document).on("click", ".remove" , function() {
+        $(this).prev().remove();
+        $(this).remove();
+    });
 });
 
 
